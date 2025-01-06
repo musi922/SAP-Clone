18cd5cf9a586928a84a35aa751f430252746134c
Richard Musime <richardmusime6@gmail.com>
2025-01-06 09:54:37 +0200

    Added the payement dialog

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -2,8 +2,28 @@ sap.ui.define(["./BaseController", "sap/m/MessageBox"], function (BaseController
 	"use strict";
 
 	return BaseController.extend("newproject.controller.Main", {
-		sayHello: function () {
-			MessageBox.show("Hello World!");
+		onPayPress(){
+			this.byId("payDialog").open();
+		},
+		onCancelPayement(){
+			this.byId("payDialog").close();
+		},
+		onConfirmPayement(){
+			const payementAmount = this.byId("payementAmount").getValue();
+			const payementMethod = this.byId("payementMethod").getSelectedKey();
+			if (!payementAmount || !payementMethod) {
+				MessageBox.error("Please enter amount and select payement method");
+			}else if(payementAmount <= 20000){
+				MessageBox.error("Amount of Money Entered is less than 20000");
+			}
+			else{MessageBox.success(
+				"Amount : " + payementAmount + " Payement Method : " + payementMethod + "Confirmed"
+			)}
+		},
+		onDialogClose(){
+			this.byId("payementAmount").setValue("");
+			this.byId("payementMethod").setSelectedKey(null);
+			this.byId("payDialog").close();
 		}
 	});
 });
